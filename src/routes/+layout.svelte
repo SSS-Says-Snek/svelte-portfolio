f29a13a271d946c48d1e4e7878111056054fe672
Brandon <bmcomis2018@gmail.com>
2023-02-22 23:57:33 -0600

    Start working on index

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <p>Snek.nekkl.de</p>
+</nav>
+
+<slot></slot>
+
+<style>
+    nav {
+        display: flex;
+
+        width: 100%;
+        background-color: #1f1f1f;
+        padding: 1.5rem;
+
+        margin-bottom: 3rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // import { fade } from 'svelte/transition';
+
+    // import PlayOnLoad from '$lib/PlayOnLoad.svelte';
+</script>
+
+<h1>SSS-Says-Snek</h1>
+
+<h2>Programming enthusiast, piano player, and e</h2>
+
+<style>
+    @keyframes fade-in {
+        from {opacity: 0}
+        50% {opacity: 1}
+        to {text-shadow: 0px 0px 50px #dee4a6;}
+    }
+
+    h1 {
+        font-size: 8rem;
+        animation: fade-in 3s ease forwards;
+    }
+</style>
